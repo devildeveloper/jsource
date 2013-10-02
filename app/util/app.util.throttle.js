f41e824abd9480704fa6bed0a243b9f0ab1417da
Brandon Kitajchuk <brandonkitajchuk@Brandon-Kitajchuk-3.local>
2013-10-02 08:19:15 -0700

    adding app.util.throttle and testing

---
+++ app/util/app.util.throttle.js
@@ -0,0 +1,43 @@
+/*!
+ *
+ * App Util: util.throttle
+ *
+ * A handly way to put a rev limiter on your event callbacks
+ *
+ * @usage: el.addEventListener( "keyup", app.util.throttle( delay, cb ), false )
+ *
+ * @deps: app
+ *
+ *
+ */
+(function ( window, app, undefined ) {
+
+
+"use strict";
+
+
+/******************************************************************************
+ * App Extensions
+*******************************************************************************/
+app.util = app.core.extend( app.util, {
+	throttle: function ( delay, callback ) {
+		var timeout = null;
+		
+		return function () {
+			var context = this,
+				args = arguments;
+			
+			clearTimeout( timeout );
+			
+			timeout = setTimeout(function () {
+				if ( typeof callback === "function" ) {
+					callback.apply( context, args );
+				}
+				
+			}, delay );
+		};
+	}
+});
+
+
+})( window, window.app );
