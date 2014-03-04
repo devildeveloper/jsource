a27301ff72e60382b54763f167ebd7342bbdcbc8
Brandon Kitajchuk <kitajchuk@gmail.com>
2014-03-04 12:44:04 -0800

    update touchme prevent default detection for horizontal swipes

--- dist/TouchMe.js
+++ dist/TouchMe.js
@@ -147,15 +147,15 @@ TouchMe.prototype = {
         }
         
         // Apply touch events, using bubbling, not capturing
-        document.addEventListener( "touchstart", function ( e ) { self._onTouchStart( e ); }, false );
-        document.addEventListener( "touchmove", function ( e ) { self._onTouchMove( e ); }, false );
-        document.addEventListener( "touchend", function ( e ) { self._onTouchEnd( e ); }, false );
+        document.addEventListener( "touchstart", function ( e ) { self._onTouchStart( this, e ); }, false );
+        document.addEventListener( "touchmove", function ( e ) { self._onTouchMove( this, e ); }, false );
+        document.addEventListener( "touchend", function ( e ) { self._onTouchEnd( this, e ); }, false );
         
         // Apply mouse events if we can, using bubbling, not capturing
         if ( !this._options.preventMouseEvents ) {
-            document.addEventListener( "mousedown", function ( e ) { self._onTouchStart( e ); }, false );
-            document.addEventListener( "mousemove", function ( e ) { self._onTouchMove( e ); }, false );
-            document.addEventListener( "mouseup", function ( e ) { self._onTouchEnd( e ); }, false );
+            document.addEventListener( "mousedown", function ( e ) { self._onTouchStart( this, e ); }, false );
+            document.addEventListener( "mousemove", function ( e ) { self._onTouchMove( this, e ); }, false );
+            document.addEventListener( "mouseup", function ( e ) { self._onTouchEnd( this, e ); }, false );
         }
     },
     
@@ -165,16 +165,19 @@ TouchMe.prototype = {
      * @memberof TouchMe
      * @method TouchMe.on
      * @param {string} event the gesture event to listen for
+     * @param {string} selector the selector to match on event
      * @param {function} handler the handler to call
      *
      */
-    on: function ( event, handler ) {
+    on: function ( event, selector, handler ) {
         if ( typeof handler === "function" ) {
             if ( !this._handlers[ event ] ) {
                 this._handlers[ event ] = [];
             }
             
             handler._timestamp = Date.now();
+            handler._selector = selector;
+            handler._eventType = event;
             
             this._handlers[ event ].push( handler );
         }
@@ -232,20 +235,135 @@ TouchMe.prototype = {
     
     /**
      *
+     * TouchMe get the angle of swipe from touchstart point to current touchmove
+     * @memberof TouchMe
+     * @method TouchMe.getSwipeAngle
+     * @param {number} x1 the last x position
+     * @param {number} x2 the current x position
+     * @param {number} y1 the last y position
+     * @param {number} y2 the current y position
+     * @returns string
+     *
+     */
+    getSwipeAngle: function ( x1, x2, y1, y2 ) {
+        var dy = y1 - y2,
+            dx = x1 - x2,
+            ret;
+            
+        ret = Math.atan2( dy, dx );
+        ret *= 180 / Math.PI;
+        
+        return ret;
+    },
+    
+    /**
+     *
+     * TouchMe determine if an event should fire
+     * @memberof TouchMe
+     * @method TouchMe.isDelegate
+     * @param {object} e the event object
+     * @param {string} selector the selector to match
+     * @returns element OR null
+     *
+     */
+    isDelegate: function ( e, selector ) {
+        return this._matchElement( e.target, selector );
+    },
+    
+    /**
+     *
+     * TouchMe determine an absolute angle is less than 45deg on horizontal axis
+     * @memberof TouchMe
+     * @method TouchMe.is45ish
+     * @param {number} a the angle to calculate with
+     *
+     */
+    is45ish: function ( a ) {
+        a = Math.abs( a );
+        
+        // Less than a 45deg angle on horizontal axis
+        return (
+            (a < 180 && a > 135) ||
+            (a > 0 && a < 45)
+        );
+    },
+    
+    /**
+     *
      * TouchMe call handlers for a gesture event
      * @memberof TouchMe
      * @method TouchMe._call
      * @param {string} eventName the gesture event to fire
+     * @param {object} eventElement the element that triggered the event
      * @param {object} eventObject the event object to pass along
      *
      */
-    _call: function ( eventName, eventObject ) {
+    _call: function ( eventName, eventElement, eventObject ) {
         if ( !this._handlers[ eventName ] ) {
             return this;
         }
         
         for ( var i = this._handlers[ eventName ].length; i--; ) {
-            this._handlers[ eventName ][ i ].call( document, eventObject );
+            var element = this.isDelegate( eventObject, this._handlers[ eventName ][ i ]._selector );
+            
+            if ( element ) {
+                this._handlers[ eventName ][ i ].call( element, eventObject );
+                
+            } else {
+                //console.log( "[TouchMe] Target element did not match this selector" );
+            }
+        }
+    },
+    
+    /**
+     *
+     * TouchMe look at all handlers and see if we should fire
+     * @memberof TouchMe
+     * @method TouchMe._isDelegated
+     * @param {object} e the event object
+     * @returns element OR null
+     *
+     */
+    _isDelegated: function ( e ) {
+        var ret = null;
+        
+        for ( var i in this._handlers ) {
+            for ( var j = this._handlers[ i ].length; j--; ) {
+                ret = this.isDelegate( e, this._handlers[ i ][ j ]._selector );
+                
+                if ( ret ) {
+                    break;
+                }
+            }
+        }
+        
+        return ret;
+    },
+    
+    /**
+     *
+     * TouchMe match an element to a selector
+     * @memberof TouchMe
+     * @method TouchMe._matchElement
+     * @param {object} el the element
+     * @param {string} selector the selector to match
+     * @returns element OR null
+     *
+     */
+    _matchElement: function ( el, selector ) {
+        var method = ( el.matches ) ? "matches" : ( el.webkitMatchesSelector ) ? "webkitMatchesSelector" : ( el.mozMatchesSelector ) ? "mozMatchesSelector" : ( el.msMatchesSelector ) ? "msMatchesSelector" : ( el.oMatchesSelector ) ? "oMatchesSelector" : null;
+        
+        // Try testing the element agains the selector
+        if ( method && el[ method ].call( el, selector ) ) {
+            return el;
+        
+        // Keep walking up the DOM if we can    
+        } else if ( el !== document.documentElement ) {
+            return this._matchElement( el.parentNode, selector );
+        
+        // Otherwise we should not execute an event    
+        } else {
+            return null;
         }
     },
     
@@ -254,10 +372,11 @@ TouchMe.prototype = {
      * TouchMe handle the touchstart event
      * @memberof TouchMe
      * @method TouchMe._onTouchStart
+     * @param {object} el the element that triggered the event
      * @param {object} e the event object passed in
      *
      */
-    _onTouchStart: function ( e ) {
+    _onTouchStart: function ( el, e ) {
         this._isTouchDown = true;
         
         // Reset
@@ -276,11 +395,21 @@ TouchMe.prototype = {
         
         if ( e.touches ) {
             this._lastTouch = e.touches.length - 1;
+            
+            // Not modified from these values
+            this._touches.x0 = e.touches[ this._lastTouch ].pageX;
+            this._touches.y0 = e.touches[ this._lastTouch ].pageY;
+            
             this._touches.x1 = e.touches[ this._lastTouch ].pageX;
             this._touches.y1 = e.touches[ this._lastTouch ].pageY;
             
         } else {
             this._lastTouch = 0;
+            
+            // Not modified from these values
+            this._touches.x0 = e.pageX;
+            this._touches.y0 = e.pageY;
+            
             this._touches.x1 = e.pageX;
             this._touches.y1 = e.pageY;
         }
@@ -291,15 +420,17 @@ TouchMe.prototype = {
      * TouchMe handle the touchmove event
      * @memberof TouchMe
      * @method TouchMe._onTouchMove
+     * @param {object} el the element that triggered the event
      * @param {object} e the event object passed in
      *
      */
-    _onTouchMove: function ( e ) {
+    _onTouchMove: function ( el, e ) {
         if ( !this._isTouchDown ) {
             return;
         }
         
-        var currSwipe,
+        var swipeAngle,
+            currSwipe,
             distX,
             distY;
         
@@ -317,6 +448,21 @@ TouchMe.prototype = {
         distX = Math.abs( this._touches.x1 - this._touches.x2 );
         distY = Math.abs( this._touches.y1 - this._touches.y2 );
         
+        swipeAngle = this.getSwipeAngle(
+            this._touches.x0,
+            this._touches.x2,
+            this._touches.y0,
+            this._touches.y2
+        );
+        
+        // Disable document scroll if horizontal swiping
+        // Accounts for matching the event element to bound event handlers
+        // Accounts for ensuring we only ever do this when the angle of swipe is below 45deg
+        // Accounts for the enabled option of preventDefault
+        if ( this._isDelegated( e ) && this.is45ish( swipeAngle ) && this._options.preventDefault ) {
+            e.preventDefault();
+        }
+        
         if ( distX > this._threshold || distY > this._threshold ) {
             currSwipe = this.getSwipe(
                 this._touches.x1,
@@ -327,11 +473,6 @@ TouchMe.prototype = {
             
             this._touches.x1 = this._touches.x2;
             this._touches.y1 = this._touches.y2;
-            
-            // Disable document scroll if horizontal swiping
-            if ( (currSwipe === "left" || currSwipe === "right") && this._options.preventDefault ) {
-                e.preventDefault();
-            }
             
             if ( currSwipe === this._lastSwipe ) {
                 return;
@@ -348,14 +489,21 @@ TouchMe.prototype = {
      * TouchMe handle the touchend event
      * @memberof TouchMe
      * @method TouchMe._onTouchEnd
+     * @param {object} el the element that triggered the event
      * @param {object} e the event object passed in
      *
      */
-    _onTouchEnd: function ( e ) {
+    _onTouchEnd: function ( el, e ) {
         this._isTouchDown = false;
         this._events.touchend = e;
         
-        var scroll = Math.abs( window.scrollY - this._windowScrollY ),
+        var swipeAngle = this.getSwipeAngle(
+                this._touches.x0,
+                this._touches.x2,
+                this._touches.y0,
+                this._touches.y2
+            ),
+            scroll = Math.abs( window.scrollY - this._windowScrollY ),
             now;
         
         // Original touchstart event
@@ -368,7 +516,7 @@ TouchMe.prototype = {
             if ( now - this._tapstart >= this._threshold ) {
                 e.gestures = ["tap"];
                 
-                this._call( "swipetap", e );
+                this._call( "swipetap", el, e );
             }
             
             this._tapstart = null;
@@ -378,7 +526,7 @@ TouchMe.prototype = {
             for ( var i = 0, len = this._gestures.length; i < len; i++ ) {
                 e.gestures = this._gestures;
                 
-                this._call( "swipe" + this._gestures[ i ], e );
+                this._call( "swipe" + this._gestures[ i ], el, e );
             }
         }
     }
